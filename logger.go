92b389ca1c3ec8bcfa8df44b61856ca7a552635b
ZafirChowdhury <zafirchowdhury69@gmail.com>
2026-06-04 14:17:00 +0600

    Add bufferd logger

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"os"
@@ -14,11 +15,12 @@ func initializeLogger() *log.Logger {
 	}
 
 	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
+	bufferedFile := bufio.NewWriterSize(file, 8192)
 	if err != nil {
 		log.Fatal(err.Error())
 		return nil
 	}
 	log.Println("LOG: LINKO_LOG_FILE found")
-	mw := io.MultiWriter(os.Stderr, file)
+	mw := io.MultiWriter(os.Stderr, bufferedFile)
 	return log.New(mw, "LOG: ", log.LstdFlags)
 }
